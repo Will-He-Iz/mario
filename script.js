61f89ca4c39df3ab20add76cbab8fb09f2702f3f
Will_He_Iz <wmgreenaway@gmail.com>
2016-01-04 21:16:31 -0500

    added bootstrap

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var main = function(){
   $(".button").click(function(){
     $(".mario").animate({
-      left: "50%"}, 600);  //animates mario running in from left //
+      left: "400px"}, 600);  //animates mario running in from left //
   });
 }
 
